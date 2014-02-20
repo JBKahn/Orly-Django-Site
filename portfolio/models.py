4702323dff202b8dde4d48ec5b9c7e1576c53611
Joseph <jkahn@waveaccounting.com>
2014-02-20 01:30:48 -0500

    harded path saving images and creating thumbnails on upload, currently exposed on portfolio page

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,49 @@
 from django.db import models
+from PIL import Image
+from cStringIO import StringIO
+from django.core.files.uploadedfile import SimpleUploadedFile
+import os
 
 
 class BridalPortfolio(models.Model):
-    imgfile = models.FileField(upload_to='portfolio/static/img')
+    imgfile = models.ImageField(upload_to='portfolio/static/img')
+    thumbnail = models.ImageField(upload_to='portfolio/static/img', max_length=500, blank=True, null=True)
+
+    def create_thumbnail(self):
+        # original code for this method came from
+        # http://snipt.net/danfreak/generate-thumbnails-in-django-with-pil/
+
+        # If there is no image associated with this.
+        # do not create thumbnail
+        if not self.imgfile:
+            return
+
+        # Set our max thumbnail size in a tuple (max width, max height)
+        THUMBNAIL_SIZE = (120, 120)
+
+        DJANGO_TYPE = self.imgfile.file.content_type
+
+        if DJANGO_TYPE == 'image/jpeg':
+            PIL_TYPE = 'jpeg'
+            FILE_EXTENSION = 'jpg'
+        elif DJANGO_TYPE == 'image/png':
+            PIL_TYPE = 'png'
+            FILE_EXTENSION = 'png'
+
+        # Open original photo which we want to thumbnail using PIL's Image
+        image = Image.open(StringIO(self.imgfile.read()))
+
+        # Use Image.ANTIALIAS to reduce potential artifacts that may result
+        image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+
+        # Save the thumbnail
+        temp_handle = StringIO()
+        image.save(temp_handle, PIL_TYPE)
+        temp_handle.seek(0)
+
+        suf = SimpleUploadedFile(os.path.split(self.imgfile.name)[-1], temp_handle.read(), content_type=DJANGO_TYPE)
+        self.thumbnail.save('%s_thumbnail.%s' % (os.path.splitext(suf.name)[0], FILE_EXTENSION), suf, save=False)
+
+    def save(self):
+        self.create_thumbnail()
+        super(BridalPortfolio, self).save()
--- portfolio/views.py
+++ portfolio/views.py
@@ -15,34 +15,7 @@ class PortfolioView(FormView):
         return super(PortfolioView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
-        return {"current_page_name": "Bridal Portfolio", "images": self.get_images(), "form": PortfolioForm(), 'uploaded': BridalPortfolio.objects.all()}
+        return {"current_page_name": "Bridal Portfolio", "images": self.get_images(), "form": PortfolioForm()}
 
     def get_images(self):
-        return [
-            {"big_path": "img/1big.png", "small_path": "img/1.png", "title": "test_title"},
-            {"big_path": "img/2big.jpeg", "small_path": "img/2.jpeg", "title": "test_title"},
-            {"big_path": "img/3big.png", "small_path": "img/3.png", "title": "test_title"},
-            {"big_path": "img/4big.jpeg", "small_path": "img/4.jpeg", "title": "test_title"},
-            {"big_path": "img/5big.jpeg", "small_path": "img/5.jpeg", "title": "test_title"},
-            {"big_path": "img/6big.jpeg", "small_path": "img/6.jpeg", "title": "test_title"},
-            {"big_path": "img/7big.png", "small_path": "img/7.png", "title": "test_title"},
-            {"big_path": "img/8big.png", "small_path": "img/8.png", "title": "test_title"},
-            {"big_path": "img/9big.png", "small_path": "img/9.png", "title": "test_title"},
-            {"big_path": "img/10big.png", "small_path": "img/10.png", "title": "test_title"},
-            {"big_path": "img/11big.jpeg", "small_path": "img/11.jpeg", "title": "test_title"},
-            {"big_path": "img/12big.jpeg", "small_path": "img/12.jpeg", "title": "test_title"},
-            {"big_path": "img/13big.jpeg", "small_path": "img/13.jpeg", "title": "test_title"},
-            {"big_path": "img/14big.jpeg", "small_path": "img/14.jpeg", "title": "test_title"},
-            {"big_path": "img/15big.png", "small_path": "img/15.png", "title": "test_title"},
-            {"big_path": "img/16big.jpeg", "small_path": "img/16.jpeg", "title": "test_title"},
-            {"big_path": "img/17big.png", "small_path": "img/17.png", "title": "test_title"},
-            {"big_path": "img/18big.jpeg", "small_path": "img/18.jpeg", "title": "test_title"},
-            {"big_path": "img/19big.jpeg", "small_path": "img/19.jpeg", "title": "test_title"},
-            {"big_path": "img/20big.png", "small_path": "img/20.png", "title": "test_title"},
-            {"big_path": "img/21big.jpeg", "small_path": "img/21.jpeg", "title": "test_title"},
-            {"big_path": "img/22big.jpeg", "small_path": "img/22.jpeg", "title": "test_title"},
-            {"big_path": "img/23big.png", "small_path": "img/23.png", "title": "test_title"},
-            {"big_path": "img/24big.png", "small_path": "img/24.png", "title": "test_title"},
-            {"big_path": "img/25big.png", "small_path": "img/25.png", "title": "test_title"},
-            {"big_path": "img/26big.png", "small_path": "img/26.png", "title": "test_title"},
-        ]
+        return [{"big_path": ('/').join(piece.imgfile.name.split('/')[-2:]), "small_path": ('/').join(piece.thumbnail.name.split('/')[-2:]), "title": "test_title"} for piece in BridalPortfolio.objects.all()]
