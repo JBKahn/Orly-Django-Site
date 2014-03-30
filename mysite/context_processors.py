c80a616f69614d9d7655f59118698dfe4fd69b8f
Joseph Kahn <josephbkahn@gmail.com>
2014-03-30 18:44:51 -0400

    changes as per talking with Orly

--- mysite/context_processors.py
+++ mysite/context_processors.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse
 
+
 def navigation(request):
     '''
     A context processor to add navigation the current Context
@@ -8,7 +9,7 @@ def navigation(request):
         {"name": "Home", "path": reverse('home:home_page')},
         {"name": "Bridal Portfolio", "path": reverse('portfolio:portfolio')},
         {"name": "Reviews", "path": reverse('reviews:reviews')},
-        {"name": "Services", "path": reverse('services:services')},
+        # {"name": "Services", "path": reverse('services:services')},
         {"name": "FX Portfolio", "path": reverse('special_effects:portfolio')},
         {"name": "The Artist", "path": reverse('artist:about')},
         {"name": "Contact", "path": reverse('contact:form')},
